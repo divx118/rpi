d372c9cd8534a085d7fa9ba246fb1a9dc124dcfb
divx118 <mauricevankruchten@gmail.com>
2015-10-31 20:38:55 +0000

    Added support if bt isn't auto connected revised the overall script.

--- audio.sh
+++ audio.sh
@@ -10,8 +10,9 @@
 # define var change to your needs.
 btdevice="C4:73:1E:0E:8B:CC"
 host="0204@localhost"
-port="900"
+port="6600"
 counter=0
+counter1=0
 timeoutcounter=200
 lircdevice="Audio"
 keypower="KEY_POWER"
@@ -19,9 +20,7 @@ keysource="satsource"
 
 poweraudio() {
 echo "Starting with power on"
-irsend send_start $lircdevice $keypower
-sleep 0.5
-irsend send_stop $lircdevice $keypower
+irsend send_once $lircdevice $keypower
 }
 
 loopsource() {
@@ -29,12 +28,13 @@ echo "Ok Now we will need a loop to walk through the inputs."
 # TODO: lower the number of times when it is more reliable.
 # We have 5 inputs.
 j=0
-while [ "x`hcitool name $btdevice`" = "x" -a $j -lt 10 ] ;do
+times=$?
+while [ "`hcitool name $btdevice`" = "" -a $j -lt $times ] ;do
   irsend send_once $lircdevice $keysource
   echo "wait a moment $j"
+  sleep 1
   j=$(( $j+1 ))
   # TODO: see if this can be speed up a bit.
-  sleep 1
 done
 }
 
@@ -43,16 +43,24 @@ poweraudio
 echo "Soundbar should be starting up now"
 echo "We will sleep for a bit to give it some time"
 sleep 2
-loopsource
+loopsource 10
 }
 
 checkstatus() {
-  if [ "x`mpc --host=$host -p $port|grep playing`" = "x" ];then
+  if [ "`mpc --host=$host -p $port|grep playing`" = "" ];then
     #playback paused or stopped we will disable output 1 (bluetooth).
     mpc --host=$host -p $port disable 1
-  elif [ "x`hcitool name $btdevice`" = "x" ];then
+    return
+  fi
+  if [ "`hcitool name $btdevice`" = "" ];then
     bluetoothaudio
-  elif [ "x`mpc --host=$host -p $port outputs|grep 'Output 1'|grep -o '[^ ]*$'`" = "xdisabled" ];then
+  fi
+  # Be sure bluetooth is connected.
+  if [ "`hcitool name $btdevice`" != "" -a "`hcitool con|grep -o \"$btdevice\"`" = "" ]; then
+    bt-audio -c "$btdevice"
+  fi
+  if [ "`mpc --host=$host -p $port outputs|grep 'Output 1'|grep -o '[^ ]*$'`" = "disabled" \
+      -a "`hcitool con|grep -o \"$btdevice\"`" = "$btdevice" ];then
     #playback started enable output 1 (bluetooth)
     mpc --host=$host -p $port enable 1
   fi
@@ -61,9 +69,21 @@ checkstatus() {
 while [ true ];do
   echo "check playback status every 2 seconds"
   checkstatus
+  if [ "`mpc --host=$host -p $port outputs|grep 'Output 1'|grep -o '[^ ]*$'`" = "disabled" ]; then
+    counter1=$(( $counter1+1 ))
+  else
+    counter1=0
+  fi
+  # Disconnect bluetooth after 5 minutes of no playback
+  if [ counter1 -eq 150 ]; then
+    counter1=0
+    if [ "`hcitool con|grep -o \"$btdevice\"`" = "$btdevice" ]; then
+      bt-audio -d $btdevice
+    fi
+  fi
   counter=$(( $counter+1 ))
-  # Empty log file every 2 hours. mpd just let's it grow.
-  if [ $counter -eq 3600 ];then
+  # Empty log file every 1 hour. mpd just let's it grow.
+  if [ $counter -eq 1800 ];then
     counter=0
     cp /var/log/mpd/mpd.log /var/log/mpd/mpd.log.old
     echo "" > /var/log/mpd/mpd.log
