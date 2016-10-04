85649a1c77995a17307344a0282538a2d3a55b07
divx118 <mauricevankruchten@gmail.com>
2016-10-04 18:04:41 +0200

    Modified the script a bit so it now should work when it is waste pickup day. Couldn't test it before, because I didn't know exactly how they did it in html.

--- check_waste.sh
+++ check_waste.sh
@@ -10,18 +10,16 @@ green=0
 blue=0
 orange=0
 day=`curl -s "$url" | hxnormalize -x | hxselect 'div.column.carouselMobile'|awk '/\<p\ class=\"/ {print $2}'|cut -d '>' -f2`
-day=`echo $day|cut -d ' ' -f1`
-if [ ! "$day" = "vandaag" ];then
-     echo "not today"
-     gpio write $led_red 0
-     gpio write $led_green 0
-     gpio write $led_blue 0
-
-     exit 0
-fi
+#day=`echo $day|cut -d ' ' -f1|cut -d '<' -f1`
+echo $day
 waste=`curl -s "$url" | hxnormalize -x | hxselect 'div.column.carouselMobile'|awk '/\<p\ class=\"/ {print $2}'|cut -d '"' -f2`
 echo $waste
-for i in $waste; do
+counter=1
+for j in $day; do
+j=`echo $j|cut -d ' ' -f1|cut -d '<' -f1`
+echo $j
+if [ "$j" = "vandaag" ];then
+i=`echo $waste|cut -d ' ' -f$counter`
     case $i in
         restafval)
             echo restafval
@@ -32,18 +30,21 @@ for i in $waste; do
             green=1
             ;;
         plastic)
-            echo gft
+            echo plastic
             blue=1
             ;;
         papier)
-            echo gft
+            echo papier
             orange=1
             ;;
         *) echo "not specified"
     esac
+fi
+counter=$(($counter+1))
+echo $counter
 done
 hour="`date +'%H'`"
-while [ "$hour" -lt 12 ]; do
+while [ "$hour" -lt 19 ]; do
     if [ $red = 1 ]; then
         gpio write $led_red 255
         sleep 1
